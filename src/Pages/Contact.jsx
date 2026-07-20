c0bc58eb94aa7a20d8de3328c5f4e60dbf675086
Huncho-BW <adenugaomoyele7@gmail.com>
2026-07-20 11:57:16 -0700

    adding EmailJs

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { MoveUpRight } from "lucide-react";
-import avater from "../assets/avater7.jpg";
-import { delay } from "framer-motion";
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
 export default function Contact() {
   const [formData, setFormData] = useState({
     userName: "",
@@ -10,29 +9,56 @@ export default function Contact() {
     discribtion: "",
   });
 
-  const [error, SetError] = useState({});
+  const [error, setError] = useState({});
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const newError = {};
 
-    if (formData.userName === "") {
-      newError.userName = "Please  Enter Your Name";
-    } else if (formData.email === "") {
+    if (formData.userName.trim() === "") {
+      newError.userName = "Please Enter Your Name";
+    } else if (formData.email.trim() === "") {
       newError.email = "Please Enter valid Email Address";
-    } else if (formData.discribtion === "") {
-      newError.discribtion = "Message should be at least 10 characters. ";
+    } else if (formData.discribtion.trim() === "") {
+      newError.discribtion = "Message should not be empty";
     }
 
-    SetError(newError);
+    setError(newError);
 
-    const element = document.getElementById("error");
-    element.scrollIntoView({
-      behavior: "smooth",
-    });
-
-    if (Object.keys(newError).length === 0) {
-      navigate("");
+    if (Object.keys(newError).length > 0) {
+      document.getElementById("error")?.scrollIntoView({
+        behavior: "smooth",
+      });
+      return;
     }
+
+    emailjs
+      .send(
+        "service_bmd8tjo",
+        "template_gfgbq1s",
+        {
+          user_name: formData.userName,
+          user_email: formData.email,
+          message: formData.discribtion,
+        },
+        {
+          publicKey: "oPGUkVQ4Lhn57aQ-o",
+        },
+      )
+      .then(() => {
+        alert("Message sent successfully 🚀");
+
+        setFormData({
+          userName: "",
+          email: "",
+          discribtion: "",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Something went wrong ❌");
+      });
   };
 
   return (
@@ -44,6 +70,7 @@ export default function Contact() {
       >
         <h1>Contact</h1>
       </motion.div>
+
       <motion.div
         initial={{ opacity: 0, y: 24 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -55,68 +82,81 @@ export default function Contact() {
           <h1 className="borderH1">Let's Create Something</h1>
           <div className="borderStyle"></div>
         </div>
-        <div className="">
+
+        <div>
           <h1 className="contactH1Font">Have a project in mind?</h1>
+
           <p className="contactPFont">
             Let's bring your ideas to life. Whether it's a website or a web app,
             I'd love to help.
           </p>
         </div>
+
         {Object.keys(error).length > 0 && (
           <div className="contactError" id="error">
             <h1>{Object.values(error)[0]}</h1>
           </div>
         )}
-        <form action="" method="get" className="contactForm">
-          <label htmlFor="" className="contactLabel">
-            What is your name
-          </label>
+
+        <form onSubmit={handleSubmit} className="contactForm">
+          <label className="contactLabel">What is your name</label>
+
           <div className="contactInput">
             <input
               type="text"
+              name="user_name"
               placeholder="John Doe"
               value={formData.userName}
-              onChange={(e) => {
-                const name = e.target.value;
-                setFormData({ ...formData, userName: name });
-              }}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  userName: e.target.value,
+                })
+              }
             />
           </div>
 
-          <label htmlFor="" className="contactLabel">
-            Your email address
-          </label>
+          <label className="contactLabel">Your email address</label>
+
           <div className="contactInput">
             <input
               type="email"
-              value={formData.emailmail}
+              name="user_email"
+              value={formData.email}
               placeholder="hello@gmail.com"
-              onChange={(e) => {
-                const email = e.target.value;
-                setFormData({ ...formData, email: email });
-              }}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  email: e.target.value,
+                })
+              }
             />
           </div>
 
-          <label htmlFor="" className="contactLabel">
-            Tell me about your project
-          </label>
+          <label className="contactLabel">Tell me about your project</label>
+
           <div className="contactTextarea">
             <textarea
+              name="message"
               value={formData.discribtion}
-              placeholder="What are you looking to build? Any specific feature or timeline "
-              onChange={(e) => {
-                const discrip = e.target.value;
-                setFormData({ ...formData, discribtion: discrip });
-              }}
+              placeholder="What are you looking to build? Any specific feature or timeline"
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  discribtion: e.target.value,
+                })
+              }
             ></textarea>
           </div>
-          <div className="contactButton" onClick={handleSubmit}>
-            <button>start your project </button>
+
+          <div className="contactButton">
+            <button type="submit">start your project</button>
           </div>
         </form>
+
         <div className="contactDividerheader">
           <div className="contactDivider"></div>
+
           <div className="contactRespon">
             <span>I'll respond within 24-48 hours</span>
           </div>
